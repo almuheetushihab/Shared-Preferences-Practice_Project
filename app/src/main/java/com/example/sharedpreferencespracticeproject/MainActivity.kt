9d069b502287f99f3a04ac55ebe77b273fa55a2b
almuheetushihab <almuheetu@gmail.com>
2025-01-19 20:56:08 +0600

    clip

--- app/src/main/java/com/example/sharedpreferencespracticeproject/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencespracticeproject/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.sharedpreferencespracticeproject
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var name: EditText
     private lateinit var age: EditText
+    private lateinit var sharedPreferences: SharedPreferencesDemo
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,23 +35,40 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        val sharedPref = getSharedPreferences("MySharedPref", MODE_PRIVATE)
-        val nameValue = sharedPref.getString("name", "")
-        val ageValue = sharedPref.getInt("age", 0)
-
+        sharedPreferences = SharedPreferencesDemo(this)
+        val (nameValue, ageValue) = sharedPreferences.getData()
+        Toast.makeText(this, "get data : $nameValue", Toast.LENGTH_SHORT)
+            .show()
         name.setText(nameValue)
         age.setText(ageValue.toString())
     }
 
     override fun onPause() {
         super.onPause()
-        val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
-        val myEdit = sharedPreferences.edit()
-
-        myEdit.putString("name", name.text.toString())
-        myEdit.putInt("age", age.text.toString().toInt())
-        myEdit.apply()
-
-
+        sharedPreferences.saveData(name.text.toString(), age.text.toString().toInt())
+        Toast.makeText(
+            this,
+            "save data : ${name.text}",
+            Toast.LENGTH_SHORT
+        ).show()
     }
+}
+
+class SharedPreferencesDemo(context: Context) {
+    private val sharedPref: SharedPreferences =
+        context.getSharedPreferences("MySharedPref", MODE_PRIVATE)
+    private val editor: SharedPreferences.Editor = sharedPref.edit()
+
+    fun saveData(name: String, age: Int) {
+        editor.putString("name", name)
+        editor.putInt("age", age)
+        editor.apply()
+    }
+
+    fun getData(): Pair<String?, Int> {
+        val name = sharedPref.getString("name", "")
+        val age = sharedPref.getInt("age", 0)
+        return Pair(name, age)
+    }
+
 }
